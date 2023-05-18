37a9e5e9a1b7a254e837b858da0ab613c818e508
igor <13igor0241@gmial.com>
2023-05-19 01:31:08 +0300

    docker db app

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask_migrate import Migrate
+from init import create_app
+from models import db
+
+app = create_app()
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+if __name__ == "__main__":
+    app.run()
