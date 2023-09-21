673543718e191263d93ec8c8fbc78036867a0482
IgorL123 <Zedenbraucht@inbox.ru>
2023-09-21 21:42:00 +0300

    threads

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = "0.0.0.0:5000"
-workers = 1 # 2 * multiprocessing.cpu_count() + 1
-threads = 4
+workers = 1
+threads = 2 * multiprocessing.cpu_count() + 1
 errorlog = "app/logs/gunicorn.log"
 preload_app = True
